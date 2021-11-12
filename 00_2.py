b411f0150c2b08b8f98876e0c65baabf57f2f5e8
PietroCappelli <92037790+PietroCappelli@users.noreply.github.com>
2021-11-12 14:03:36 +0100

    consegna esercizi 12_11

--- 00_2.py
+++
@@ -1,7 +0,0 @@
-def swap(x,y):
-    x,y=y,x
-    return print(x,y)
-
-swap("ciao",100)
-
-#controlla se il tipo di variabile è int, char,..
--- 02_7.py
+++
@@ -1,9 +0,0 @@
-from math import sqrt
-def f(x):
-    sqrt(1-x**2)
-N=100
-h = 2/N
-I=0
-
-for i in range(1,N+1):
-    I += h*f(i)
