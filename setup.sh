da6eb004330426f7329110fb702ef0e6a711619e
PhysWiza <daniel.harley.jordan@outlook.com>
2021-12-20 16:44:14 +0100

    standard push

---
+++ setup.sh
@@ -0,0 +1,14 @@
+git clone https://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@github.com/PhysWiza/LaboratoryOfComputationalPhysics_Y4.git
+
+cd LaboratoryOfComputationalPhysics_Y4/
+
+git config --global user.name "PhysWiza"
+
+git config --global user.email "daniel.harley.jordan@outlook.com"
+
+git remote add upstream https://github.com/PhysicsOfData/LaboratoryOfComputationalPhysics_Y4.git
+
+git fetch upstream
+
+git checkout -b dev upstream/Jordan_Daneil-Harley
+
